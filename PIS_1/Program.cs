0dd0cb06426dcdf10acf8e57a3dc08dc139253e1
adomin00 <adomin00@fesb.hr>
2015-03-12 11:20:28 +0100

    promjena

--- PIS_1/Program.cs
+++ PIS_1/Program.cs
@@ -10,6 +10,7 @@ namespace PIS_1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello world!!!");
+            Console.WriteLine("bla bla");
             Console.Read();
         }
     }
